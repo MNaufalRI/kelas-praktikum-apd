34df4bb8631cec269e8c24ef7d7c5ff59558dbc0
Muhammad Naufal Rifyan Ilham <mnaufal2408@gmail.com>
2024-10-09 08:51:58 +0800

    pertemuan6
    Nama = Muhammad Naufal Rifyan Ilham
    NIM = 2409106034
    Kelas = A2'24
    pertemuan6

---
+++ kelas-praktikum-apd/Pertemuan-6/2409106034_MuhammadNaufalRifyanIlham_A2_pertemuan6.py
@@ -0,0 +1,156 @@
+# daftar_buku = {
+# "Buku1" : "Harry Potter",
+# "Buku2" : "Percy Jackson",
+# "Buku3" : "Twillight"
+# }
+# print(daftar_buku["Buku1"])
+# print(daftar_buku["Buku2"])
+# print(daftar_buku["Buku3"])
+
+# daftar_buku ={}
+
+# daftar_buku["Buku1"] = "Harry Potter"
+
+# print(daftar_buku)
+
+# Biodata = {
+# "Nama" : "Aldy Ramadhan Syahputra",
+# "NIM" : 2109106079,
+# "KRS" : ["Program Web", "Struktur Data", "Basis Data"],
+# "Mahasiswa_Aktif" :True,
+# "Social Media" : {
+#     "Instagram" : "@aldyrmdhns_",
+#     "Discord" : "\'Izanami#6848"
+#     }
+# }
+
+# # print(Biodata["Social Media"]["Discord"])
+# # print(Biodata["KRS"][1])
+# print(Biodata.get('KRS'))
+
+# Nilai = {
+# "Matematika" : 80,
+# "B. Indonesia" : 90,
+# "B. Inggris" : 81,
+# "Kimia" : 78,
+# "Fisika" : 80
+# }
+
+# #tanpa menggunakan items
+# for i in Nilai:
+#     print(i)
+# print("")
+# #menggunakan items
+# for mapel,nilai in Nilai.items():
+#     print(f"Nilai {mapel} anda adalah {nilai}")
+
+# Nilai["Sejarah"]=100
+# print(Nilai["Sejarah"])
+# Nilai.update({"Biologi" : 87})
+# print(Nilai)
+
+# del Nilai["Kimia"]
+
+# simpan = Nilai.pop("Fisika")
+# print(Nilai)
+# print(simpan)
+
+# print("jumlah data : ", len(Nilai))
+
+# Buku = {
+# "No Longer Human" : "Osamu Dazai",
+# "Harry Potter" : "J.K. Rowlings",
+# "Hamlet" : "William Shakespeare"
+# }
+# pinjam = Buku.copy()
+# print("Dictionary yang Telah Disalin : ", pinjam)
+
+# key = "apel", "jeruk", "mangga"
+# value = 1, 2, 3
+# buah = dict.fromkeys(key, value)
+# print(buah)
+
+# Nilai = {
+# "Matematika" : 80,
+# "B. Indonesia" : 90,
+# "B. Inggris" : 81,
+# "Kimia" : 78,
+# "Fisika" : 80
+# }
+# print("")
+# #menggunakan keys
+# for i in Nilai.keys():
+#     print(i)
+# print("")
+# #menggunakan value
+# for i in Nilai.values():
+#     print(i)
+
+# Nilai = {
+# "Matematika" : 80,
+# "B. Indonesia" : 90,
+# "B. Inggris" : 81
+# }
+# #sebelum Setdefault
+# print(Nilai)
+# print("")
+# #menggunakan setdefault
+# print("Nilai : ", Nilai.setdefault("Kimia", 70))
+# print("")
+# #setelah menggunakan setdefault
+# print(Nilai)
+
+# Musik = {
+# "The Chainsmoker" : ["All we Know", "The Paris"],
+# "Alan Walker" : ["Alone", "Lily"],
+# "Neffex" : ["Best of Me", "Memories"]
+# }
+# for i, j in Musik.items():
+#     print(f"Musik milik {i} adalah : ")
+#     for song in j:
+#         print(song)
+#     print("")
+
+# mahasiswa = {
+# 101 : {"Nama" : "Aldy", "Umur" : 19},
+# 111 : {"Nama" : "Abdul", "Umur" : 18}
+# }
+
+# for i, j in mahasiswa.items():
+#     print(f"ID Mahasiwa: ", i)
+#     for key,value in j.item:
+#         print()
+#     print("")
+
+# mahasiswa = {
+# 101 : {"Nama" : "Aldy", "Umur" : 19},
+# 111 : {"Nama" : "Abdul", "Umur" : 18}
+# }
+# #Sebelum Dilakukan Perubahan
+# print(mahasiswa)
+
+# #Menambahkan Item pada Nested Dictionary
+# mahasiswa[101]["Angkatan"] = 2023
+# print(mahasiswa)
+
+# #Mengubah Item pada Nested Dictionary
+# mahasiswa[101]["Nama"] = "Rizal"
+# print(mahasiswa)
+
+# #Menghapus Item pada Nested Dictionary
+# del mahasiswa[101]["Umur"]
+# print(mahasiswa)
+
+# data = {
+# 1 : {"Nama" : "p","Umur " : "24"}
+# }
+# input("Ubah Nama : ")
+
+# data = {
+#     "Matematika" : 90,
+#     "Fisika" : 80,
+#     "Biologi" : 80,
+#     "Kimia" : 70,
+# }
+
+# print(f"Total Nilai")
